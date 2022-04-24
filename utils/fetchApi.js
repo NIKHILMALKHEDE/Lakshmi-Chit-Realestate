66a9501c2d3b782f8682f4393dee9ce33d361e0b
NIKHILMALKHEDE <malkhedenikhil@gmail.com>
2022-04-24 14:51:44 +0530

    .env added

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,8 +5,8 @@ export const baseUrl = "https://bayut.p.rapidapi.com";
 export const fetchApi = async (url) => {
   const {data} = await axios.get(url, {
     headers: {
-      "X-RapidAPI-Host": "bayut.p.rapidapi.com",
-      "X-RapidAPI-Key": "518191f8d8msh43fe2b4cd2db50bp144aabjsnf9ef56e68261",
+      "X-RapidAPI-Host":process.env.NEXT_APP_HOST,
+      "X-RapidAPI-Key": process.env.NEXT_APP_KEY,
     }
   });
 
